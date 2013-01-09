ff825ba80f466ff37cffcd7227ad9c10218c9d79
Bin Wang <wbin00@gmail.com>
2013-01-09 16:41:14 +0800

    draft for rc.lua

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,5 +1,6 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
+local naughty = require("naughty")
 
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 modkey = "Mod4"
@@ -49,16 +50,53 @@ taglist.buttons = awful.util.table.join(
 	awful.button({ }, 4, awful.tag.viewnext),
 	awful.button({ }, 5, awful.tag.viewprev)
 )
+tasklist = {}
+tasklist.buttons = awful.util.table.join(
+	awful.button({ }, 1, function (c)
+		if c == client.focus then
+			c.minimized = true
+		else
+			if not c:isvisible() then
+				awful.tag.viewonly(c:tags()[1])
+			end
+			-- This will also un-minimize
+			-- the client, if needed
+			client.focus = c
+			c:raise()
+		end
+	end),
+	awful.button({ }, 3, function ()
+		if instance then
+			instance:hide()
+			instance = nil
+		else
+			instance = awful.menu.clients({ width=250 })
+		end
+	end),
+	awful.button({ }, 4, function ()
+		awful.client.focus.byidx(1)
+		if client.focus then client.focus:raise() end
+	end),
+	awful.button({ }, 5, function ()
+		awful.client.focus.byidx(-1)
+		if client.focus then client.focus:raise() end
+	end)
+)
 
 statusbar = {}
 for s = 1, screen.count() do
 	-- create taglist
 	taglist[s] = awful.widget.taglist(s, awful.widget.taglist.lable.all, taglist.bottons)
+	-- create tasklist
+	tasklist[s] = awful.widget.tasklist(function (c)
+		return awful.widget.tasklist.lable.currenttags(c, s)
+	end, tasklist.buttons)
 
 	statusbar[s] = awful.wibox({ position="bottom", screen=s })
 	statusbar[s].widgets = {
 		{
-			taglist,
+			taglist[s],
+			tasklist[s],
 			layout = awful.widget.layout.horizontal.leftright
 		},
 		textclock,
@@ -69,14 +107,35 @@ end
 -- }}}
 
 -- {{{ key bindings
-globalkeys = awful.util.table.join(
+root.keys(awful.util.table.join(
 	awful.key({modkey,    }, "h", awful.tag.viewprev),
 	awful.key({modkey,    }, "l", awful.tag.viewnext),
+	awful.key({modkey,    }, "Tab", function ()
+		awful.client.focus.history.previous()
+		if client.focus then
+			client.focus:raise()
+		end
+	end),
 
 	-- program
 	awful.key({modkey,    }, "return", function ()
 		awful.util.spawn(terminal) end)
-)
-root.keys(globalkeys)
--- }}}}
+))
+-- }}}
 
+-- {{{ mouse bindings
+root.buttons(awful.util.table.join(
+	aweful.button({}, 3, function () mainmenu:toggle() end)
+))
+-- }}}
+
+-- {{{ menu
+awesomemenu = {
+	{ "restart", awesome.restart },
+	{ "quit", awesome.quit}
+}
+mainmenu = awful.menu({ items = {
+	{"awesome", awesomemunu, beautiful.awesome_icon},
+	{"open terminal", terminal}
+}})
+-- }}}
