8a0ae5ddbe2e571f2753516d59770f5c6909da46
Bin Wang <wbin00@gmail.com>
2013-01-09 16:01:10 +0800

    Add statubar for awesome

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -2,12 +2,11 @@ local awful = require("awful")
 local beautiful = require("beautiful")
 
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+modkey = "Mod4"
 
 terminal = "konsol"
 editor = "vim"
 editor_cmd = terminal .. " -e " .. editor
-
-modkey = "Mod4"
 
 local layouts = {
 	awful.layout.suit.floating,
@@ -24,9 +23,9 @@ local layouts = {
 	awful.layout.suit.magnifier
 }
 
--- {{{ Setting tags
+-- {{{ tags
 tags = {
-	names = {"term", "www", "chat", "mail", "work", "media", "other"},
+	names = { "term", "www", "chat", "mail", "work", "media", "other" },
 	layout = {
 		layouts[10], layouts[10], layouts[1],
 		layouts[10], layouts[2], layouts[10], layouts[2]
@@ -37,7 +36,39 @@ for s = 1, screen.count() do
 end
 -- }}}
 
--- {{{ Key bindings
+-- {{{ statusbar and widget on it
+-- widgets
+textclock = awful.widget.textclock({ align = "right" })
+systray = widget({ type = "systray" })
+taglist = {}
+taglist.buttons = awful.util.table.join(
+	awful.button({ }, 1, awful.tag.viewonly),
+	awful.button({ modkey }, 1, awful.client.movetotag),
+	awful.button({ }, 3, awful.tag.viewtoggle),
+	awful.button({ modkey }, 3, awful.client.toggletag),
+	awful.button({ }, 4, awful.tag.viewnext),
+	awful.button({ }, 5, awful.tag.viewprev)
+)
+
+statusbar = {}
+for s = 1, screen.count() do
+	-- create taglist
+	taglist[s] = awful.widget.taglist(s, awful.widget.taglist.lable.all, taglist.bottons)
+
+	statusbar[s] = awful.wibox({ position="bottom", screen=s })
+	statusbar[s].widgets = {
+		{
+			taglist,
+			layout = awful.widget.layout.horizontal.leftright
+		},
+		textclock,
+		systray,
+		layout = awful.widget.layout.horizontal.rightleft
+	}
+end
+-- }}}
+
+-- {{{ key bindings
 globalkeys = awful.util.table.join(
 	awful.key({modkey,    }, "h", awful.tag.viewprev),
 	awful.key({modkey,    }, "l", awful.tag.viewnext),
