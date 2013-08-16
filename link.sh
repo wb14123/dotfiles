12ffd956e704f16702f7f161fc3c2595f0d2ee3e
Bin Wang <wbin00@gmail.com>
2013-08-16 10:11:58 +0800

    Add mysql config

--- link.sh
+++ link.sh
@@ -22,3 +22,6 @@ check_link git/gitconfig     .gitconfig
 check_link git/global_ignore .global_ignore
 check_link xprofile          .xprofile
 check_link tmux.conf         .tmux.conf
+# mysql config, see https://github.com/nitso/colour-mysql-console
+check_link mysql/grcat       .grcat
+check_link mysql/my.cnf      .my.cnf
